e393fcb8b560d0ccbc93b8b394ccf1d9280944bf
Deptl <pateldeep312000@gmail.com>
2026-04-05 16:39:13 -0400

    added url mapping and shortning feature

--- src/main/java/com/example/link_snap/controller/UrlMappingController.java
+++ src/main/java/com/example/link_snap/controller/UrlMappingController.java
@@ -1,18 +1,17 @@
 package com.example.link_snap.controller;
 
 import com.example.link_snap.dto.UrlMappingDto;
+import com.example.link_snap.model.UrlMapping;
 import com.example.link_snap.model.User;
 import com.example.link_snap.service.UrlMappingService;
 import com.example.link_snap.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -30,5 +29,14 @@ public class UrlMappingController {
         User user = userService.findByUsername(principal.getName());
         UrlMappingDto urlMappingDto = urlMappingService.createShortUrl(originalUrl, user);
         return ResponseEntity.ok(urlMappingDto);
+    }
+
+    @GetMapping("/myurls")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<UrlMappingDto>> getUserUrls(Principal principal){
+        User user = userService.findByUsername(principal.getName());
+        List<UrlMappingDto> urls =  urlMappingService.getUrlsByUser(user);
+        return ResponseEntity.ok(urls);
+
     }
 }
